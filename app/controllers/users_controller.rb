277736d389fd527027d241a1b39fbe14282600d4
Rails udemy course <mayurpathak888@gmail.com>
2019-05-28 15:40:13 -0700

    registration form create email pass username

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "successfully Signup Welcome To #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+
+
+  def edit
+  end
+
+  def _form
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'users/_form'
+  # get 'users/update'
   root 'welcome#home'
   get 'welcome/about'
   # get 'dogs/index'
@@ -8,5 +14,7 @@ Rails.application.routes.draw do
   # get 'dogs/edit'
   resources :dogs
   resources :articles
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
