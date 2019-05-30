9dd34a33c5d7f2f5bc51cb8069a0a15f8848e584
Rails udemy course <mayurpathak888@gmail.com>
2019-05-30 15:21:35 -0700

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "successfully Signup Welcome To #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
